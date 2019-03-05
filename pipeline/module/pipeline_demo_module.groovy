3dce68013bc865d8138db07c485006fb07ebb00d
Evan0302 <944672405@qq.com>
2019-03-05 18:17:28 +0800

    训练第二篇002

--- pipeline/module/pipeline_demo_module.groovy
+++ pipeline/module/pipeline_demo_module.groovy
@@ -1,20 +1,12 @@
-pipeline {
-	agent any
-	stages {
-		stage('Build') {
-			steps {
-				println "Build"
-			}
-		}
-		stage('Test') {
-			steps {
-				println "Test"
-			}
-		}
-		stage('Deploy') {
-			steps {
-				println "Deploy"
-			}
-		}
+import hudson.model.*;
+ 
+ 
+def find_files(filetype) {
+	
+	def files = findFiles(glob:filetype)
+	for (file in files) {
+		println file.name
 	}
 }
+ 
+return this;
--- pipeline/src/pipeline_script_job.groovy
+++ pipeline/src/pipeline_script_job.groovy
@@ -1,29 +1,27 @@
 import hudson.model.*;
-
+ 
 println env.JOB_NAME
 println env.BUILD_NUMBER
 println env.WORKSPACE
-
+ 
 pipeline{
-   
-   agent any
-   stages{
-	   stage("init") {
-		   steps{
-			   script{
-				 
-				   model_test = load env.WORKSPACE + "/pipeline/module/pipeline_demo_module.groovy"
-			   }
-		   }
-	   }
-	   stage("Test Method") {
-		   steps{
-			   script{
-				   log_files = model_test.find_files('**/*.log')
-			   }
-		   }
-	   }
-   }
+	agent any
+	stages{
+		stage("init") {
+			steps{
+				script{
+					model_test = load env.WORKSPACE + "/pipeline/module/pipeline_demo_module.groovy"
+				}
+			}
+		}
+		stage("Test Method") {
+			steps{
+				script{
+					log_files = model_test.find_files('**/*.log')
+				}
+			}
+		}
+	}
 }
-
-
+ 
+ 
