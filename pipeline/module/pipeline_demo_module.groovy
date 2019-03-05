9c9c05f9dff738c11e26a5c99b9a3bd18ff5c490
Evan0302 <944672405@qq.com>
2019-03-05 11:01:04 +0800

    修改构建脚本01

--- pipeline/module/pipeline_demo_module.groovy
+++ pipeline/module/pipeline_demo_module.groovy
@@ -1,11 +1,20 @@
-
-
-    /**
-    * @ClassName: pipeline_demo_module
-    * @Description: TODO(这里用一句话描述这个类的作用)
-    * @date 2019年3月4日
-    *
-    */
-
-class pipeline_demo_module {
+pipeline {
+	agent any
+	stages {
+		stage('Build') {
+			steps {
+				println "Build"
+			}
+		}
+		stage('Test') {
+			steps {
+				println "Test"
+			}
+		}
+		stage('Deploy') {
+			steps {
+				println "Deploy"
+			}
+		}
+	}
 }
