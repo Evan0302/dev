1a3479c60fc95c2cf8f931884db6f276b699a57a
Evan0302 <944672405@qq.com>
2019-03-07 19:42:59 +0800

    增加判断文件是否存在的方法 fileExists

--- pipeline/src/pipeline_script_job_json.groovy
+++ pipeline/src/pipeline_script_job_json.groovy
@@ -84,6 +84,21 @@ pipeline{
 			   }
 		   }
 	   }
+	  
+	   stages{
+		   stage("fileExists") {
+			   steps{
+				   script {
+					   json_file = "${env.WORKSPACE}/testdata/test_json.json"
+					   if(fileExists(json_file) == true) {
+						   echo("json file is exists")
+					   }else {
+						   error("here haven't find json file")
+					   }
+				   }
+			   }
+		   }
+	   }
    }
 }
 
