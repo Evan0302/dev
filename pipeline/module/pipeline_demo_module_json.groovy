c21986bd0b9d28760306edebea2bed62fd0d8b3c
Evan0302 <944672405@qq.com>
2019-03-07 09:42:31 +0800

    增加json打印

---
+++ pipeline/module/pipeline_demo_module_json.groovy
@@ -0,0 +1,25 @@
+import hudson.model.*;
+
+
+def find_files(filetype) {
+   
+   def files = findFiles(glob:filetype)
+   for (file in files) {
+	   println file.name
+   }
+}
+
+def read_json_file(file_path) {
+   def propMap = readJSON file : file_path
+   propMap.each {
+	   println ( it.key + " = " + it.value )
+   }
+}
+
+def read_json_file2(json_string) {
+   def propMap = readJSON text : json_string
+   propMap.each {
+	   println ( it.key + " = " + it.value )
+   }
+}
+return this;
---
+++ pipeline/src/pipeline_script_job_json.groovy
@@ -0,0 +1,31 @@
+import hudson.model.*;
+
+println env.JOB_NAME
+println env.BUILD_NUMBER
+
+pipeline{
+   
+   agent any
+   stages{
+	   stage("init") {
+		   steps{
+			   script{
+				   model_test = load env.WORKSPACE + "/pipeline/module/pipeline_demo_module.groovy"
+			   }
+		   }
+	   }
+	   stage("Parse json") {
+		   steps{
+			   script{
+				   json_file = env.WORKSPACE + "/testdata/test_json.json"
+				   model_test.read_json_file(json_file)
+				   println "================================"
+				   json_string = '{"NAME":"Anthony","AGE":18,"CITY":"Beijing","GENDER":"male"}'
+				   model_test.read_json_file2(json_string)
+			   }
+		   }
+	   }
+   }
+}
+
+
